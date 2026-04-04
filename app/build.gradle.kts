c63a6428beeb88f6c1dacc12a701d39582139e2d
evan8686 <evan8686@gmail.com>
2026-04-05 03:13:34 +0800

    Update build.gradle.kts
    1.2.0更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.evan8686.eewreceiver"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.5"
+        versionCode = 5
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
