2d9d4e9731f08ebac27c5d43f424655bbbc84e3f
evan8686 <7821415+evan8686@users.noreply.github.com>
2026-06-17 01:40:57 +0800

    V2.1.0 正式版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.evan8686.eewreceiver"
         minSdk = 24
         targetSdk = 36
-        versionCode = 15
-        versionName = "2.0.0"
+        versionCode = 16
+        versionName = "2.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
